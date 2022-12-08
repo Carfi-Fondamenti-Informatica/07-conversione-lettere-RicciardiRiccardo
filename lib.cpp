3a1b67d7fd17f5c9ff2a8358d025407a3557a354
RicciardiRiccardo <117997389+RicciardiRiccardo@users.noreply.github.com>
2022-12-08 13:13:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
 bool funzione (char carattere) {
-    if ((((char)97 <= carattere) and (carattere <= (char)122)) or (((char)65 <= carattere) and (carattere <= (char)90))){
+    if ((((char)97<=carattere) and (carattere<=(char)122)) or (((char)65<=carattere) and (carattere<=(char)90))){
         return true;
     }
     else {
@@ -10,7 +10,7 @@ bool funzione (char carattere) {
 }
 
 char conversione (char carattere){
-    if (('a' <= carattere) and (carattere <= 'z')) {
+    if (('a'<=carattere) and (carattere<='z')) {
         carattere-=(char)32;
     }
     else {
